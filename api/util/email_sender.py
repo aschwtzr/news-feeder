a8a62c1298bd1af9b78f48782e6957527dbfc905
aschwtzr <albert@owal.io>
2020-02-03 21:35:44 -0500

    clean up email

--- api/util/email_sender.py
+++ api/util/email_sender.py
@@ -9,7 +9,7 @@ email = os.environ.get('EMAIL_ADDRESS')
 url = 'http://127.0.0.1:5000'
 
 default_sources = requests.get(url + '/sources')
-contents = []
+contents = ['<body>']
 briefings = requests.get(url + '/briefings?limit=6')
 google = requests.get(url + '/google-news?limit=6')
 #print(briefings.text)
@@ -18,15 +18,18 @@ print("running")
 for source in parsed_source_results:
     head = source["source"]
     summary = source["summary"]
-    contents.append(f"<h2>{head}</h2>")
+    # print(head)
+    # print(summary)
+    head_string = f"<h2>{head}</h2>"
+    contents.append(head_string)
     contents.append(f"{summary}")
     for article in source["articles"]:
         title = article["title"]
         date = article["date"]
         url = article["url"]
         preview = article["preview"]
-        contents.append(f"<h3>{title}</h3>")
-        contents.append(f"<h4>{date}</h4>")
+        contents.append(f"<strong>{title}</strong>")
+        contents.append(f"{date}")
         contents.append(f"{url}")
         contents.append(f"{preview}")
         contents.append("<br>")
