cbc87bca2ac1ee3d21d9ba76db16150710a16569
Albert Schweitzer <albert@phosphorus.com>
2022-05-18 00:33:20 -0400

    fix time

--- api/fix_content_loop.py
+++ api/fix_content_loop.py
@@ -2,6 +2,8 @@ from feeder.formatter.topic_mapper import map_articles
 from feeder.formatter.content_fixer import extract_nlp_summ_kw, fix_most_recent, extract_missing_features, fetch_articles_missing
 from feeder.models.article import Article
 from feeder.util.time_tools import print_timestamp, date_time_string
+import time
+startTime = time.time()
 
 while True:
   hours_ago_date_time = date_time_string(48)
@@ -26,5 +28,5 @@ while True:
     except:
       print_timestamp("All caught up, congrats!")
       time.sleep(5)
-
-  print_timestamp("FINISHED THE LOOP DEE LOOP")
+  executionTime = (time.time() - startTime)
+  print_timestamp(f"FINISHED THE LOOP DEE LOOP {executionTime}")
