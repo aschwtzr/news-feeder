8c2dd8bb254970c003a3f028bcb8d3230c2639df
Albert Schweitzer <albert@phosphorus.com>
2022-06-06 19:49:16 -0400

    remove react frontend

--- react-web/src/modules/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import briefingReducer from './briefingReducer'
-import newsReducer from './newsReducer'
-
-export default combineReducers({
-  briefing: briefingReducer,
-  news: newsReducer,
-})
